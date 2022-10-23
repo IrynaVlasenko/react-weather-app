324210e667936510064237adc738c8c58fda21a3
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-10-23 13:10:34 +0300

    added git repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Iryna Vlasenko and is {""}
+        <a href="https://github.com/IrynaVlasenko/react-weather-app">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
