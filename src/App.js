daf143389e0c20c3cd23b521b2c6aec1e6c1bb05
IrynaVlasenko <106557356+IrynaVlasenko@users.noreply.github.com>
2022-10-24 21:08:42 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-export default function App() {
-  const apiKey = "fb4ad69a2bb4c6370849b9a18c3de8e4";
-  let city = "Detroit";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <h2>TEXAS</h2>
-            <h1>
-              <span></span>
-              <span className="degrees">
-                <a href="/" className="active">
-                  19°C
-                </a>
-                |<a href="/">33°F</a>
-              </span>
-              <br />
-              <img
-                src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sun_weather_icon_150657.png"
-                alt=""
-              ></img>
-            </h1>
-            <form>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search"
-                autoComplete="off"
-              />
-              <button type="button" className="btn btn-primary">
-                {" "}
-                Search
-              </button>
-            </form>
-            <p>
-              Last updated:<span>Saturday</span>
-            </p>
-            <p>Snowing</p>
-            <p>
-              Humidity <span>77</span>%{" "}
-            </p>
-            <p>
-              Wind <span>30</span>km/h{" "}
-            </p>
+export default function App(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      //   coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Monday 07:00",
+      //   date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: "https://cdn.icon-icons.com/icons2/2505/PNG/512/sun_weather_icon_150657.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <h2>{weatherData.city}</h2>
+              <h1>
+                <span>{Math.round(weatherData.temperature)}</span>
+                <span className="degrees">
+                  <a href="/" className="active">
+                    °C
+                  </a>
+                  |<a href="/">33°F</a>
+                </span>
+                <br />
+                <img src={weatherData.icon} alt=""></img>
+              </h1>
+              <form>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search"
+                  autoComplete="off"
+                />
+                <button type="button" className="btn btn-primary">
+                  {" "}
+                  Search
+                </button>
+              </form>
+              <p>
+                Last updated:<span>Saturday</span>
+              </p>
+              <p className="text-capitalize">{weatherData.description}</p>
+              <p>
+                Humidity <span>{weatherData.humidity}</span>%{" "}
+              </p>
+              <p>
+                Wind <span>{weatherData.wind}</span>km/h{" "}
+              </p>
+            </div>
           </div>
+          <footer>
+            This project was coded by Iryna Vlasenko and is {""}
+            <a href="https://github.com/IrynaVlasenko/react-weather-app">
+              open-sourced on GitHub
+            </a>
+          </footer>
         </div>
-        <footer>
-          This project was coded by Iryna Vlasenko and is {""}
-          <a href="https://github.com/IrynaVlasenko/react-weather-app">
-            open-sourced on GitHub
-          </a>
-        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "fb4ad69a2bb4c6370849b9a18c3de8e4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
